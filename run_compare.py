4b8acd2ccbbfcc47004a0e6833170bd8590d2f45
tomiko <igrro2@ac.sce.ac.il>
2018-04-25 09:55:38 +0300

    add 2 files

---
+++ run_compare.py
@@ -0,0 +1,6 @@
+#!/bin/python
+
+compareFiles = ("fgp2.fasta", "9802.fasta", "6803.fasta", "29413.fasta", "7120.fasta", "7942.fasta")
+matchType = ("m", "m", "nm", "nm", "nm", "nm")
+initialFile = "leptolyngbya.fasta"
+totalCompares = len(compareFiles)
