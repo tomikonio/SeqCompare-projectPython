25765bdc6317b616d41d621c490f319175b2fc2d
tomiko <igrro2@ac.sce.ac.il>
2018-04-25 10:19:28 +0300

    Add some python to run_compare

--- run_compare.py
+++ run_compare.py
@@ -1,6 +1,15 @@
 #!/bin/python
 
-compareFiles = ("fgp2.fasta", "9802.fasta", "6803.fasta", "29413.fasta", "7120.fasta", "7942.fasta")
-matchType = ("m", "m", "nm", "nm", "nm", "nm")
-initialFile = "leptolyngbya.fasta"
-totalCompares = len(compareFiles)
+
+# files to be compared with
+compare_files = {"fgp2.fasta": "m", "9802.fasta": "m", "6803.fasta": "nm", "29413.fasta": "nm", "7120.fasta": "nm",
+                 "7942.fasta": "nm"}
+# match_type = ("m", "m", "nm", "nm", "nm", "nm")
+initial_file = "leptolyngbya.fasta"
+
+total_compares = len(compare_files)
+current_compare = 1
+input_protein = initial_file
+
+for input_nucl in compare_files:
+    print(input_nucl)
