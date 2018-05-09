ef3607e00505878f52d798581118642942cffc54
tomiko <igrro2@ac.sce.ac.il>
2018-05-09 15:31:34 +0300

    Add table columns

--- compare.py
+++ compare.py
@@ -1,11 +1,19 @@
 from Bio import SeqIO
 from Bio import SearchIO
+from flask_table import Table, Col
+from beautifultable import BeautifulTable
 import sys
+
+table = BeautifulTable()
+table.column_headers = ["Query_Name", "Query_Length", "Query_Cover_Length", "Cover_Perc Acc_No", "Length", "Desc",
+                        "E_Value", "Bit_Score", "Frame", "QStart", "QEnd", "Hit_Start", "Hit_End",
+                        "Positives Identical"]
+print(table)
 
 input_file = sys.argv[1]
 compare_file = sys.argv[2]
 
-input_handle = open(input_file)
-for query_result in SearchIO.parse(input_handle, format='blast-xml'):
-    # TODO how to
-    print()
+# input_handle = open(input_file)
+# for query_result in SearchIO.parse(input_handle, format='blast-xml'):
+#     # TODO better use the the BLAST libraries in order to parse the blast outputs
+#     print()
