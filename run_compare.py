63d2f351aaa2678634b692f51f96e7d24a7fb3dd
igor <igrr69@gmail.com>
2018-05-14 12:54:13 +0300

    Add BLAST commands, almost complete the set_cmd function

--- run_compare.py
+++ run_compare.py
@@ -1,6 +1,7 @@
 #!/bin/python
+import os
 
-
+dir = os.getcwd()
 # files to be compared with
 compare_files = {"fgp2.fasta": "m", "9802.fasta": "m", "6803.fasta": "nm", "29413.fasta": "nm", "7120.fasta": "nm",
                  "7942.fasta": "nm"}
@@ -13,3 +14,21 @@ input_protein = initial_file
 
 for input_nucl in compare_files:
     print(input_nucl)
+    print(compare_files[input_nucl])
+
+
+
+def set_cmd():
+    new_dir = "{}/compare{}".format(dir, current_compare)
+
+    if current_compare == 1:
+        os.mkdir(new_dir)
+
+    # Todo copies
+
+    os.chdir(new_dir)
+
+    makeblastdb_cline = "makeblastdb -in {} -dbtype nucl".format(input_nucl)
+    tblastn_cline = "tblastn -query {} -db {} -out compare_output{}.txt".format(input_protein, input_nucl,
+                                                                                current_compare)
+    next_input = "matching_protein_fasta" if compare_files[input_nucl] == "m" else "not_matching_protein_fasta"
