435a9245623a3bcc579f95f56a0cfd5a9d17398b
igor <igrr69@gmail.com>
2018-05-20 12:28:43 +0300

    Add the first main "if" of the compare file

--- compare.py
+++ compare.py
@@ -29,7 +29,7 @@ def push_to_match_hash(key):
     match_dict[key] = "1"
 
 
-def push_to_not_match_hasg(key):
+def push_to_not_match_dict(key):
     not_match_dict[key] = "0"
 
 
@@ -50,9 +50,44 @@ def sequence_manipulations(key, query_length, output_file, alphabet):
         SeqIO.write(seq_obj, output_file, "fasta")
 
 
-
 def make_compare():
-    print()
+    FILEHANDLE1.write(columns)
+    FILEHANDLE2.write(columns)
+
+    push_fasta_contigs_to_dict()
+
+    for query_result in input_protein:
+        query_name = query_result.id
+        query_description = query_result.description
+        full_name = "{} {}".format(query_name, query_description)
+
+        # output "no hits found" if there is no hits
+        if len(query_result.hit_keys) == 0:
+            # if this protein already exists as not matched, write it to repeating_protein.txt
+            if full_name in not_match_dict:
+                FILEHANDLE3.write("{}\t".format(full_name))
+                FILEHANDLE3.write("\n")
+            else:
+                global count_found_no_hits
+                count_found_no_hits += 1
+                FILEHANDLE2.write("{} {}---===***No Hits Found***===---\n".format(full_name, len(query_result)))
+                # Todo check if len(query_result) is the same as $result->query_length in perl
+
+                # write to dict of all not matching
+                push_to_not_match_dict(full_name)
+
+                # write to not_matching_protein_fasta
+                sequence_manipulations(full_name, len(query_result), out_file_not_matching, alphabet)
+
+        # hits found
+        else:
+            temp_evalue = -1
+            temp_hsp = ""
+            hit = next(query_result.hits)
+            # Todo check if next(query_result.hits) is the same as $result->next_hit in perl
+
+
+
 
 
 protein_dict = {}
@@ -75,6 +110,8 @@ compare_file = sys.argv[2]
 #     # TODO better use the the BLAST libraries in order to parse the blast outputs
 #     print()
 
+columns = "Query_Name Query_Length Query_Cover_Length Cover_Perc Acc_No Length Desc E_Value Bit_Score Frame QStart QEnd Hit_Start Hit_End Positives Identical"
+
 input_protein = SearchIO.parse(input_file, format="blast-xml")
 progress = 0
 count_found_hits = 0
@@ -88,7 +125,7 @@ try:
     with open("matching_protein.txt", "w") as FILEHANDLE1, open("not_matching_protein.txt", "w") as FILEHANDLE2, open(
             "sorted_not_matching_protein.txt", "w") as FILEHANDLE3, open("repeating_protein.txt", "w") as FILEHANDLE4:
 
-        make_compare()
+        make_compare(count_found_no_hits)
 
 except IOError as e:
     print("Operation failed: {}".format(e.strerror))
